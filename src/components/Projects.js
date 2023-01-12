cac66cda3d705e2e8b54ca4554fe135081a05e12
Fred Hsieh <fhsieh@uci.edu>
2023-01-11 17:59:35 -0800

    Update project and skills

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,154 +7,177 @@ import nodeBlog from "../images/blog.png";
 import uCCompiler from "../images/compiler.png";
 import nodeBBS from "../images/bbs.png";
 import portfolioImage from "../images/portfolio.png";
+import jobAgg from "../images/Linkedin.png";
 
 const useStyles = makeStyles((theme) => ({
-  section: {
-    backgroundColor: "rgba(255, 255, 255, .2)",
-    backdropFilter: "blur(45px)",
-    paddingTop: "8rem",
-    paddingBottom: "8rem",
-    margin: "auto",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    textAlign: "center",
-    display: "flex",
-  },
-  imageContainer: {
-    position: "relative",
-    display: "flex",
-    justifyContent: "center",
-    "&:hover $imageOverlay": {
-      opacity: 1,
+    section: {
+        backgroundColor: "rgba(255, 255, 255, .2)",
+        backdropFilter: "blur(45px)",
+        paddingTop: "8rem",
+        paddingBottom: "8rem",
+        margin: "auto",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+        display: "flex",
     },
-  },
-  responsiveImg: {
-    width: "100%",
-    height: "auto",
-    display: "flex",
-  },
-  imageOverlay: {
-    position: "absolute",
-    width: "100%",
-    height: "100%",
-    backgroundColor: "#007cc7",
-    color: "#eefbfb",
-    opacity: 0,
-    transition: "0.3s",
-  },
-  overlayItems: {
-    position: "relative",
-    top: "25%",
-    padding: "1rem",
-  },
-  overlayTitle: {
-    fontSize: "2rem",
-    fontFamily: "Lato",
-    [theme.breakpoints.down("xs")]: {
-      fontSize: "1.2rem",
+    imageContainer: {
+        position: "relative",
+        display: "flex",
+        justifyContent: "center",
+        "&:hover $imageOverlay": {
+            opacity: 1,
+        },
     },
-  },
-  overlayText: {
-    fontSize: "1rem",
-    fontFamily: "Lato",
-    marginTop: "1rem",
-    [theme.breakpoints.down("xs")]: {
-      fontSize: "0.5rem",
+    responsiveImg: {
+        width: "100%",
+        height: "auto",
+        display: "flex",
     },
-  },
-  sectionHeading: {
-    color: "#4da8da",
-    margin: "auto",
-    textAlign: "center",
-    paddingBottom: "120px",
-  },
-  emphasize: { color: "#eefbfb" },
+    imageOverlay: {
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#007cc7",
+        color: "#eefbfb",
+        opacity: 0,
+        transition: "0.3s",
+    },
+    overlayItems: {
+        position: "relative",
+        top: "25%",
+        padding: "1rem",
+    },
+    overlayTitle: {
+        fontSize: "2rem",
+        fontFamily: "Lato",
+        [theme.breakpoints.down("xs")]: {
+            fontSize: "1.2rem",
+        },
+    },
+    overlayText: {
+        fontSize: "1rem",
+        fontFamily: "Lato",
+        marginTop: "1rem",
+        [theme.breakpoints.down("xs")]: {
+            fontSize: "0.5rem",
+        },
+    },
+    sectionHeading: {
+        color: "#4da8da",
+        margin: "auto",
+        textAlign: "center",
+        paddingBottom: "120px",
+    },
+    emphasize: { color: "#eefbfb" },
 }));
 
 const portfolioData = [
-  {
-    url: nftMarket,
-    title: "NFT Marketplace",
-    link: "https://github.com/fchsieh/MyNFT-Market",
-  },
-  {
-    url: nodeBlog,
-    title: "Node.JS Blog",
-    link: "https://github.com/fchsieh/nodejs_blog",
-  },
-  {
-    url: uCCompiler,
-    title: "uC Compiler",
-    link: "https://github.com/fchsieh/uC-Compiler",
-  },
-  {
-    url: nodeBBS,
-    title: "Node.JS BBS",
-    link: "https://github.com/fchsieh/Node-BBS",
-  },
-  {
-    url: portfolioImage,
-    title: "My Portfolio",
-    link: "https://portfolio.fredhs.dev",
-  },
+    {
+        url: jobAgg,
+        title: "Job Aggregator",
+        link: "http://job-agg.shion.live",
+    },
+    {
+        url: nftMarket,
+        title: "NFT Marketplace",
+        link: "https://github.com/fchsieh/MyNFT-Market",
+    },
+    {
+        url: nodeBlog,
+        title: "Node.JS Blog",
+        link: "https://github.com/fchsieh/nodejs_blog",
+    },
+    {
+        url: uCCompiler,
+        title: "uC Compiler",
+        link: "https://github.com/fchsieh/uC-Compiler",
+    },
+    {
+        url: nodeBBS,
+        title: "Node.JS BBS",
+        link: "https://github.com/fchsieh/Node-BBS",
+    },
+    {
+        url: portfolioImage,
+        title: "My Portfolio",
+        link: "https://portfolio.fredhs.dev",
+    },
 ];
 
 const Projects = () => {
-  const classes = useStyles();
-  return (
-    <>
-      <Box className={classes.section}>
-        <Container>
-          <Grid container>
-            <Grid item xs={12}>
-              <Box>
-                <Typography variant="h3" className={classes.sectionHeading}>
-                  Recent <span className={classes.emphasize}>Projects</span>
-                </Typography>
-              </Box>
-            </Grid>
-          </Grid>
-          <Container maxWidth="lg">
-            <Grid
-              container
-              spacing={2}
-              style={{ justifyContent: "center", alignItems: "center" }}
-            >
-              {portfolioData.map((item, i) => (
-                <Grid item xs={12} lg={4} key={i}>
-                  <Box className={classes.imageContainer}>
-                    <img
-                      src={item.url}
-                      alt={item.title}
-                      className={classes.responsiveImg}
-                    />
-                    <Box className={classes.imageOverlay}>
-                      <Box className={classes.overlayItems}>
-                        <Typography className={classes.overlayTitle}>
-                          {item.title}
-                        </Typography>
-
-                        <Button
-                          variant="contained"
-                          className={classes.overlayText}
-                          href={item.link}
-                          component="a"
-                          target="_blank"
+    const classes = useStyles();
+    return (
+        <>
+            <Box className={classes.section}>
+                <Container>
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <Box>
+                                <Typography
+                                    variant="h3"
+                                    className={classes.sectionHeading}
+                                >
+                                    Recent{" "}
+                                    <span className={classes.emphasize}>
+                                        Projects
+                                    </span>
+                                </Typography>
+                            </Box>
+                        </Grid>
+                    </Grid>
+                    <Container maxWidth="lg">
+                        <Grid
+                            container
+                            spacing={2}
+                            style={{
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
                         >
-                          Visit
-                        </Button>
-                      </Box>
-                    </Box>
-                  </Box>
-                </Grid>
-              ))}
-            </Grid>
-          </Container>
-        </Container>
-      </Box>
-    </>
-  );
+                            {portfolioData.map((item, i) => (
+                                <Grid item xs={12} lg={4} key={i}>
+                                    <Box className={classes.imageContainer}>
+                                        <img
+                                            src={item.url}
+                                            alt={item.title}
+                                            className={classes.responsiveImg}
+                                        />
+                                        <Box className={classes.imageOverlay}>
+                                            <Box
+                                                className={classes.overlayItems}
+                                            >
+                                                <Typography
+                                                    className={
+                                                        classes.overlayTitle
+                                                    }
+                                                >
+                                                    {item.title}
+                                                </Typography>
+
+                                                <Button
+                                                    variant="contained"
+                                                    className={
+                                                        classes.overlayText
+                                                    }
+                                                    href={item.link}
+                                                    component="a"
+                                                    target="_blank"
+                                                >
+                                                    Visit
+                                                </Button>
+                                            </Box>
+                                        </Box>
+                                    </Box>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Container>
+                </Container>
+            </Box>
+        </>
+    );
 };
 
 export default Projects;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,6 +9,7 @@ import pythonLogo from "../images/python.png";
 import cppLogo from "../images/cpp.png";
 import jsLogo from "../images/js.png";
 import bashLogo from "../images/bash.png";
+import goLogo from "../images/golang.png";
 // libraries
 import reactLogo from "../images/react.png";
 import nodeLogo from "../images/node.png";
@@ -21,112 +22,114 @@ import latexLogo from "../images/latex.png";
 import mongoLogo from "../images/mongo.png";
 
 const useStyles = makeStyles((theme) => ({
-  section: {
-    backgroundColor: "rgba(0, 124, 199, 0.4)",
-    backdropFilter: "blur(45px)",
-    paddingTop: "8rem",
-    paddingBottom: "8rem",
-    margin: "auto",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    textAlign: "center",
-    display: "flex",
-  },
-  sectionHeading: {
-    color: "#eefbfb",
-    margin: "auto",
-    textAlign: "center",
-    paddingBottom: "60px",
-  },
-  emphasize: {
-    color: "#4da8da",
-  },
-  tab: {
-    color: "#eefbfb",
-  },
+    section: {
+        backgroundColor: "rgba(0, 124, 199, 0.4)",
+        backdropFilter: "blur(45px)",
+        paddingTop: "8rem",
+        paddingBottom: "8rem",
+        margin: "auto",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+        display: "flex",
+    },
+    sectionHeading: {
+        color: "#eefbfb",
+        margin: "auto",
+        textAlign: "center",
+        paddingBottom: "60px",
+    },
+    emphasize: {
+        color: "#4da8da",
+    },
+    tab: {
+        color: "#eefbfb",
+    },
 }));
 
 const skillsData = [
-  { label: "C/C++", image: cppLogo },
-  { label: "Python", image: pythonLogo },
-  { label: "JavaScript", image: jsLogo },
-  { label: "Bash", image: bashLogo },
+    { label: "C/C++", image: cppLogo },
+    { label: "Python", image: pythonLogo },
+    { label: "JavaScript", image: jsLogo },
+    { label: "Go", image: goLogo },
+    { label: "Bash", image: bashLogo },
 ];
 
 const librariesData = [
-  { label: "React", image: reactLogo },
-  { label: "Node", image: nodeLogo },
-  { label: "Keras", image: kerasLogo },
+    { label: "React", image: reactLogo },
+    { label: "Node", image: nodeLogo },
+    { label: "Keras", image: kerasLogo },
 ];
 
 const toolsData = [
-  { label: "Linux", image: linuxLogo },
-  { label: "Git", image: gitLogo },
-  { label: "Docker", image: dockerLogo },
-  { label: "MongoDB", image: mongoLogo },
-  { label: "LaTeX", image: latexLogo },
+    { label: "Linux", image: linuxLogo },
+    { label: "Git", image: gitLogo },
+    { label: "Docker", image: dockerLogo },
+    { label: "MongoDB", image: mongoLogo },
+    { label: "LaTeX", image: latexLogo },
 ];
 
 const Skills = () => {
-  const [tab, setTab] = useState("1");
+    const [tab, setTab] = useState("1");
 
-  const classes = useStyles();
-  return (
-    <>
-      <Box className={classes.section}>
-        <Box>
-          <Typography variant="h3" className={classes.sectionHeading}>
-            Professional <span className={classes.emphasize}>Skills</span>
-          </Typography>
-        </Box>
-        <TabContext value={tab}>
-          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-            <TabList
-              onChange={(e, value) => {
-                setTab(value);
-              }}
-              variant="center"
-            >
-              <Tab
-                className={classes.tab}
-                label="Languages"
-                value="1"
-                style={{
-                  fontSize: "1rem",
-                }}
-              />
-              <Tab
-                className={classes.tab}
-                label="Libraries"
-                value="2"
-                style={{
-                  fontSize: "1rem",
-                }}
-              />
-              <Tab
-                className={classes.tab}
-                label="Tools"
-                value="3"
-                style={{
-                  fontSize: "1rem",
-                }}
-              />
-            </TabList>
-          </Box>
-          <TabPanel value="1">
-            <SkillTab data={skillsData} />
-          </TabPanel>
-          <TabPanel value="2">
-            <SkillTab data={librariesData} />
-          </TabPanel>
-          <TabPanel value="3">
-            <SkillTab data={toolsData} />
-          </TabPanel>
-        </TabContext>
-      </Box>
-    </>
-  );
+    const classes = useStyles();
+    return (
+        <>
+            <Box className={classes.section}>
+                <Box>
+                    <Typography variant="h3" className={classes.sectionHeading}>
+                        Professional{" "}
+                        <span className={classes.emphasize}>Skills</span>
+                    </Typography>
+                </Box>
+                <TabContext value={tab}>
+                    <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                        <TabList
+                            onChange={(e, value) => {
+                                setTab(value);
+                            }}
+                            variant="center"
+                        >
+                            <Tab
+                                className={classes.tab}
+                                label="Languages"
+                                value="1"
+                                style={{
+                                    fontSize: "1rem",
+                                }}
+                            />
+                            <Tab
+                                className={classes.tab}
+                                label="Libraries"
+                                value="2"
+                                style={{
+                                    fontSize: "1rem",
+                                }}
+                            />
+                            <Tab
+                                className={classes.tab}
+                                label="Tools"
+                                value="3"
+                                style={{
+                                    fontSize: "1rem",
+                                }}
+                            />
+                        </TabList>
+                    </Box>
+                    <TabPanel value="1">
+                        <SkillTab data={skillsData} />
+                    </TabPanel>
+                    <TabPanel value="2">
+                        <SkillTab data={librariesData} />
+                    </TabPanel>
+                    <TabPanel value="3">
+                        <SkillTab data={toolsData} />
+                    </TabPanel>
+                </TabContext>
+            </Box>
+        </>
+    );
 };
 
 export default Skills;
